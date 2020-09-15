661a216df6ca6a9bf440dbdfead57b8b9eaea58b
hangil-jung <hangil.jung@samsung.com>
2020-09-15 17:06:45 +0900

    post proxy added. TODO: basename resolve issue

--- web-proxy.js
+++ web-proxy.js
@@ -5,10 +5,9 @@ const app = express();
 const port = 3000;
 const DEST = "http://notion.so";
 let getRequest = (reqUrl, callback) => {
-	console.log(`URL is ${reqUrl}`);
+	console.log(`GET ${reqUrl}`);
 	let url = new URL(reqUrl);
 	let client = '';
-	console.log(url.protocol);
 	if (url.protocol === 'http:') {
 		client = http;
 	} else if (url.protocol === 'https:') {
@@ -25,14 +24,6 @@ let getRequest = (reqUrl, callback) => {
 			case 2:
 				console.log("Success Response!");
 				callback(res);
-				// res.on('data', (d) => {
-				// 	// callback(d);
-				// 	// process.stdout.write(d);
-				// 	console.log('some data!');
-				// });
-				// res.on('close', () => {
-				// 	console.log('send fin!');
-				// })
 				break;
 			case 3:
 				let location = res.headers.location;
@@ -48,13 +39,56 @@ let getRequest = (reqUrl, callback) => {
 		console.error(e);
 	});
 };
-app.get('/*', (req, res) => {
+let postRequest = (reqUrl, callback) => {
+	console.log(`POST ${reqUrl}`);
+	let url = new URL(reqUrl);
+	let client = '';
+	if (url.protocol === 'http:') {
+		client = http;
+	} else if (url.protocol === 'https:') {
+		client = https;
+	}
+	client.request({
+		hostname: url.host,
+		port: url.port,
+		path: url.pathname,
+		method: "POST",
+		agent: false
+	}, (res)=> {
+		let statusCode = res.statusCode;
+		switch(parseInt(statusCode/100)) {
+			case 2:
+				console.log("Success Response!");
+				callback(res);
+				break;
+			case 3:
+				let location = res.headers.location;
+				console.log(`Redirection to ...${location}`);
+				getRequest(location, callback);
+				break;
+			default:
+				console.log(`Status Code is ${statusCode}`);
+				break;
+		}
+		
+	}).on('error', (e) => {
+		console.error(e);
+	});
+};
+app.get('*', (req, res) => {
 	let path = req.path;
 	let userAgent = req.headers['user-agent'];
 	getRequest(`${DEST}${path}`, (d) => {
 		d.pipe(res);
 	});
-})
+	
+});
+app.post('*', (req, res) => {
+	let path = req.path;
+	postRequest(`${DEST}${path}`, (d) => {
+		d.pipe(res);
+	});
+});
 
 app.listen(port, () => {
   console.log(`Proxy app listening at http://localhost:${port}`);
